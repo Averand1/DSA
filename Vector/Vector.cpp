d9f3359d740edc5576db95f83fbcaf8fba4f79aa
Averand1 <leonk8867@gmail.com>
2021-10-19 10:27:43 -0400

    Vector array resizing itself

--- Vector/Vector.cpp
+++ Vector/Vector.cpp
@@ -10,7 +10,18 @@ int main() {
     //Creating vector
     vector<int> v1 = {1,2,3,4}; //vector<DataType> VariableName = {Elements of the said DataType}
     v1.push_back(5);
+    v1.push_back(5);
+    v1.push_back(5);
+    v1.push_back(5);
+    cout<<v1.capacity()<<endl; 
+    cout<<v1.size()<<endl;
+    v1.pop_back();
+    v1.pop_back();
+    v1.pop_back();
+    v1.pop_back();
     cout<<v1.capacity()<<endl; //The capacity increased by 4 when we tried add 5
+    cout<<v1.size()<<endl;
+    //What happens is that once the vector is not big enough, it doubles
 }   
 
 
