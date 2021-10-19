da957e394b8d373484d5806c230bb5a719b501ba
Averand1 <leonk8867@gmail.com>
2021-10-19 10:41:54 -0400

    Insert and delete using begin

--- Vector/Vector.cpp
+++ Vector/Vector.cpp
@@ -9,20 +9,12 @@ using namespace std;
 int main() {
     //Creating vector
     vector<int> v1 = {1,2,3,4}; //vector<DataType> VariableName = {Elements of the said DataType}
-    v1.push_back(5);
-    v1.push_back(5);
-    v1.push_back(5);
-    v1.push_back(5);
-    cout<<v1.capacity()<<endl; 
-    cout<<v1.size()<<endl;
-    v1.pop_back();
-    v1.pop_back();
-    v1.pop_back();
-    v1.pop_back();
-    v1.shrink_to_fit(); //Capacity changes from 8 to 4
-    cout<<v1.capacity()<<endl; //The capacity increased by 4 when we tried add 5
-    cout<<v1.size()<<endl;
-    //What happens is that once the vector is not big enough, it doubles
+    //Insert and Erase elements
+    v1.insert(v1.begin(), 5); //Insert element at the beginning
+    cout<<v1[0]<<endl;
+    v1.erase(v1.begin()); //Delete the first element
+    cout<<v1[0]<<endl;
+
 }   
 
 
