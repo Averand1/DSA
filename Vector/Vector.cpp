1c6f31fa0ee51bfaf4d9bfdd6d44d424f06006f7
Averand1 <leonk8867@gmail.com>
2021-10-19 10:11:51 -0400

    Access vector elements

---
+++ Vector/Vector.cpp
@@ -0,0 +1,15 @@
+/*Vectors are just dynamic arrays that can change it's size
+if you try to add more elements it will just increase it size*/
+//Normal arrays have certain limit
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    //Creating vector
+    vector<int> v1 = {1,2,3}; //vector<DataType> VariableName = {Elements of the said DataType}
+    cout <<v1[1]<<endl; //Access vector elements
+}   
+
+
