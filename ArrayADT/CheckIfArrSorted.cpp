214b0ac020fc9294eea47d7603255a0182779e39
Averand1 <leonk8867@gmail.com>
2021-10-24 12:35:59 -0400

    Check if array is sorted corrected

--- ArrayADT/CheckIfArrSorted.cpp
+++ ArrayADT/CheckIfArrSorted.cpp
@@ -6,41 +6,27 @@ struct Array
     int size;
     int length;
 };
-
-void Display(struct Array arr)
+    void Display(struct Array arr)
     {
         int i;
         printf("\nElements are\n");
         for(i=0;i<arr.length;i++)
             printf("%d ",arr.A[i]);
     }
-void swap(int *x,int *y)
+int isSorted(struct Array arr)
+{
+    int i;
+    for(i=0;i<arr.length-1;i++)
     {
-        int temp=*x;
-     *x=*y;
-     *y=temp;
-    }
-int InsertSorted(struct Array *arr,int x) //Can insert elements in sorted position
-    {
-        int i=arr->length-1;
-        if(arr->length==arr->size)
+        if(arr.A[i]>arr.A[i+1])
             return 0;
-        while(i>=0 && arr->A[i]>x)
-        {
-            arr->A[i+1]=arr->A[i];
-            i--;
-        }
-        arr->A[i+1]=x;
-        arr->length++; 
-        return 0; 
-        }
-
-
+    }
+    return 1;
+}
 int main()
 {
     struct Array arr1={{2,3,9,16,18,21,28,32,35},10,9};
-
-    InsertSorted(&arr1, 1);
+    printf("%d",isSorted(arr1));
     Display(arr1);
     return 0;
 }
