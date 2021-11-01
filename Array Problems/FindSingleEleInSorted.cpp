61eecf4b79d572b9b4c8553351f4ed1f3dfbe9fb
Averand1 <leonk8867@gmail.com>
2021-11-01 13:43:12 -0400

    n changed to size div

--- Array Problems/FindSingleEleInSorted.cpp
+++ Array Problems/FindSingleEleInSorted.cpp
@@ -4,7 +4,7 @@
     {
     int A[N]= {1,2,3,4,5,6,8,9,10,11,12};
     int sum=0; // declaration missing
-    int n = 12; 
+    int n = sizeof(A)/sizeof(A[0]); 
     int s;
     for(int i=0; i<11; i++)
     {
