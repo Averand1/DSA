d9dc6f6b98c54f1fb208cc74b916c2d4920b88b3
Averand1 <leonk8867@gmail.com>
2021-11-06 06:59:00 -0400

    Explanation added

--- Array Problems/FindSingleEleInSorted.cpp
+++ Array Problems/FindSingleEleInSorted.cpp
@@ -1,16 +1,16 @@
     #include<stdio.h>
-    #define N 12 // size should be 12
+    #define N 12 
     int main()
     {
     int A[N]= {1,2,3,4,5,6,8,9,10,11,12};
-    int sum=0; // declaration missing
+    int sum=0; //setting sum to 0
     int n = sizeof(A)/sizeof(A[0]); 
     int s;
-    for(int i=0; i<11; i++)
+    for(int i=0; i<11; i++) //terating till last element
     {
-    sum=sum+A[i];
+    sum=sum+A[i]; //adding A[i] to sum till the loop ends
     };
-    s=n*(n+1)/2;
-    printf("The number is %d ",s-sum);
+    s=n*(n+1)/2;  // 1 + 2 + .. + n = n(n+1)/2 [no number is missing element here]
+    printf("The number is %d ",s-sum); //difference S-sum is the missing element
     return 0;
     }
