c04c9c6a9316bf09fa5c1f0a6192663be61d2373
Averand1 <leonk8867@gmail.com>
2021-11-06 07:06:22 -0400

    Added a brief explanation of Bubble Sort

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+//for this example
+/* In Bubble Sort we compare the the first element with the next and if the first element 
+is larger than next(second) element we swap their position*/
 
 void swap(int *x, int *y)  //swap function to change the elements at certain indexes
 {
@@ -15,7 +18,7 @@ void Bubble(int A[], int n) //taking an array input and it's size
         flag=0; //setting flag 0 everytime
         for(j=0; j<n-i-1; j++) //loop n-1-i as the no of elements to sort reduce after each iteration
         {
-            if(A[j]>A[j+1]) //if current element is greater than the one ahead
+            if(A[j]<A[j+1]) //if current element is greater than the one ahead
             {
                 swap(&A[j], &A[j+1]); //we swap it's position
                 flag=1; //as we swapped the position, we set flag to 1
