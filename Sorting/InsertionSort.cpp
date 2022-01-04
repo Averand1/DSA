e61b9242000b8264492bd6771718884efe95a0cb
Averand1 <leonk8867@gmail.com>
2022-01-04 13:35:41 +0530

    Added explanation for Insertion and Selecton sort

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -13,19 +13,19 @@ void Print(T& vec, int n, string s) { //takes all types of arguments
     cout<<"]"<<endl;
 }
 
-void Insertion(int A[], int n) {
-    int i,j,x;
-    for(i=1; i<n; i++)
+void Insertion(int A[], int n) { //Insertion Sort
+    int i,j,x; //Initializing i,j,x
+    for(i=1; i<n; i++) //Starting the array from i=1 as we need something to compare bfr it
     {
-        j=i-1;
-        x=A[i];
-        while(j>-1&&A[j]>x)
+        j=i-1; //we'll let j start from behind i
+        x=A[i]; //element which we want in sorted position
+        while(j>-1&&A[j]>x) //once A[j] becomes greater than element we want in sorted positn 
         {
-            A[j+1]=A[j];
+            A[j+1]=A[j]; //we'll shift the element by one to the right
             j--;
         }
-        A[j+1]=x;
-    }
+        A[j+1]=x; //and at j+1 we'll copy our x which we'll be in sorted position
+    } //Repeat this process till whole array is sorted
 }
 int main() {
     int A[] = {3,7,9,10,6,5,12,4,11,2};
